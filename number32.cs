129010067b3008ba6e39a349c244dc2d46156b0d
mariealyuossif-collab <mariealyuossif@gmail.com>
2026-01-22 15:00:59 +0200

    Add files via upload

---
+++ number32.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    internal class number
+    {
+        public int x;
+        public float c;
+        public number()
+        {
+            this.x = 12;
+            this.c = 17;
+        }
+        public void show()
+        {
+            Console.WriteLine("int number" + this.x + "float nuber" + this.c);
+        }
+        public number(int v1, float v2)
+        {
+            this.x = v1;
+            this.c = v2;
+        }
+
+
+    }
+}
---
+++ rogramme32.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            number n1 = new number(20,40);
+            number n2 = new number(12,15);
+            
+            n1.show();
+        }
+    }
+}
