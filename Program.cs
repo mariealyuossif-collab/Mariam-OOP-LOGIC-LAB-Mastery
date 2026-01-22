aac1a0e436581f0e2b9ff2e3b3d623d2f2b74d51
mariealyuossif-collab <mariealyuossif@gmail.com>
2026-01-22 14:04:55 +0200

    "Upload ConsoleApp31-OOP Constructors Implementation"
    " In this project, I created a 'number' class with fields (x, y, name) . I implemented Constructor Overloading to handle different data inputs and displayed the results in Main method ."

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            number n1 = new number(13,12,"mariam");
+            number n2 = new number();
+            number n3 = new number("ahmed");
+            n1.name = "mariam";
+            Console.WriteLine(n3.x +" " +  n3.x + " " + n3.name);
+            
+          
+            
+
+        }
+    }
+}
---
+++ number.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp31
+{
+    internal class number
+    {
+        public int x;
+        public int y;
+        public string name;
+        public number()
+        {
+            x = 0;
+            y = 0;
+            name = "null";
+        }
+        public number(int v1, int v2, string c)
+        {
+            x = v1;
+            y = v2;
+            name = c;
+        }
+        public number(string s)
+        {
+            name = s;
+            x = 15;
+            y = 16;
+        }
+        
+
+    }
+}
