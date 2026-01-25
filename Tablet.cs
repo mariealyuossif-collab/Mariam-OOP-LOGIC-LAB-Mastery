557d604b867a2a3de64d3e3b6cfe6388a0827bc2
mariealyuossif-collab <mariealyuossif@gmail.com>
2026-01-25 19:39:51 +0200

    Add files via upload

---
+++ Tablet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp41
+{
+    internal class Tablet: medicine
+    {
+        public int pillscount {  get; set; }
+        public void calculatepillprice()
+        {
+            pillscount = 15;
+            Price = 19.5;
+            double r = Math.Round(Price / pillscount);
+            Console.WriteLine(r * 100 + "%");   
+        }
+    }
+}
---
+++ medicine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp41
+{
+    internal class medicine
+    {
+        public string name;
+        private double price;
+        public int qauntity;
+        public medicine()
+        {
+            Console.WriteLine("A drug was added to the system");
+        }
+        public string Name { get; set; }
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                if (value > 0)
+                {
+                    price = value;
+                }
+                else
+                {
+                    price = 5;
+                }
+            }
+        }
+        public void printdata()
+        {
+            Console.WriteLine("Name: " + this.name);
+            Console.WriteLine("Price: " + this.price);
+        }
+                
+
+
+
+    }
+}
